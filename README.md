f9ec0743089b7aad76fc81e1449f8bb74408a374
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-04-20 08:02:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jdbc1
+ Studying database operations using MySQL and JDBC
---
+++ src/db/DB.java
@@ -0,0 +1,59 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	/*
+	 * the connection to the database
+	 */
+	private static Connection conn = null;
+
+	/*
+	 * will return the connection to the database it will also connect to the
+	 * database in case that the connection object is null
+	 */
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	/*
+	 * Will read the properties from the db.properties file the method will load it
+	 * and return an object Properties with the information
+	 */
+	private static Properties loadProperties() {
+
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+}
